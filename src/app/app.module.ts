7aa782f00f6b2ffc70db360335950aa7df150797
vaibhav7920 <vaibhav.parashar@protiviti.co.in>
2023-12-08 23:40:29 +0530

    done till basics - selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+  //  selector : '[app-server]', //component can be selected as an attribute
+   // selector : '.app-server', //component can be selected as a class
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+
+}
