b1e8475a51af4da26fe7ad4bc2b10a345be5a341
vaibhav7920 <vaibhav.parashar@protiviti.co.in>
2023-12-09 11:58:55 +0530

    property bining and string intrapolatin

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
-
+ serverId =10;
+ serverStatus = "offline";
+ getserverStatus(){
+  return this.serverStatus;
+ }
 }
