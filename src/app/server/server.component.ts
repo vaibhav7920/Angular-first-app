92adbd7020b9cba5197b6caac56b8ac700f4ef52
vaibhav7920 <vaibhav.parashar@protiviti.co.in>
2023-12-11 15:14:22 +0530

    basics module completed

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,12 +4,22 @@ import { Component } from '@angular/core';
     selector: 'app-server',
   //  selector : '[app-server]', //component can be selected as an attribute
    // selector : '.app-server', //component can be selected as a class
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles:[`
+    .online {
+      color:white;
+      }`]
 })
 export class ServerComponent{
  serverId =10;
- serverStatus = "offline";
+ serverStatus : string="offline";
+ constructor(){
+  this.serverStatus =Math.random() >0.5 ? 'online':'offline';
+ }
  getserverStatus(){
   return this.serverStatus;
+ }
+ getColor(){
+  return this.serverStatus=='online'? 'green':'red';
  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
 allowNewServer =false;
 serverCreationStaus ='No Server was created';
 serverName ='ADSS';
+serverCreated = false;
+servers = ['TestServer 1', 'TestServer 2'];
   constructor(){
     setTimeout(() => {
       this.allowNewServer =true;
@@ -18,9 +20,30 @@ serverName ='ADSS';
   }
   ngOnInit() {
   }
+
   onCreateServer(){
-    this.serverCreationStaus ='Server was created';
+    this.serverCreated=true;
+    this.servers.push(this.serverName);
+    this.serverCreationStaus ='Server was created! Name is '+ this.serverName ;
   }
+
+    // onCreateServer(){
+  //   this.serverCreated=true;
+  //   this.serverCreationStaus ='Server was created! Name is '+ this.serverName ;
+  // }
+
+
+    // onCreateServer(){
+  //     this.serverCreationStaus ='Server was created! Name is '+ this.serverName ;
+  //   }
+
+
+  // onCreateServer(){
+  //   this.serverCreationStaus ='Server was created';
+  // }
+
+
+
   onUpdateServerName(event : Event){
 this.serverName = (<HTMLInputElement>event.target).value;
   }
