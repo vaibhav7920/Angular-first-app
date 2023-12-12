4da1aa5c80947be7da0123824a57fe9c417ec4be
vaibhav7920 <vaibhav.parashar@protiviti.co.in>
2023-12-12 19:51:47 +0530

    type Script basics

---
+++ src/app/typescript/basics.ts
@@ -0,0 +1,61 @@
+//Primitives - number, string, boolean
+
+let age : number
+age =12; // can be assigned integer
+age =235.678;  //// can be assigned float
+
+let num : number =14; // can be assigned when declared
+
+let num1 :Number; // this points at number object and not primitive data type
+
+let userName: string;
+userName= 'Vaibhav';
+
+let isInstructor : boolean;
+isInstructor=true;
+
+
+/*
+
+COMPLEX  DATA TYPES
+
+*/
+
+let hobbies : string[]; // array of string
+hobbies = ['Sports', 'Cooking'];
+
+
+/*  OBJECT */ 
+
+let person: { // this the way to tell typeScript the data types that can be stored in an object
+    name: string;
+    age :number;
+ }
+
+ person ={          // person can only store the string and number data types with variables name and age as specifed in declaration
+    name: "roshnan",
+    age:45,
+ }
+
+/* Array of objects */
+ let people:{ 
+    name: string;
+    age :number;
+ }[];     // Array of objects
+
+
+/* ANY  DATA TYPE Start  */
+
+let student :any; // any is the default type
+ student ={
+    name: "max",
+    age: 10
+ }
+
+ student={        // any type can store any data which is like javascript
+    isStudent:false
+ }
+
+
+
+ 
